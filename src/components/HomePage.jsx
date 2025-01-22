420ea636551b0d15ab88d474fe3f29721520a90a
DeepanSenthilkumar005 <deepancse45@gmail.com>
2025-01-22 17:52:31 +0530

    feat:Icon added in the Home Page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,49 @@
 import { useState } from 'react';
 import ProfilePhoto from "../assets/person.avif"
-
+import { SlSocialLinkedin } from "react-icons/sl";
+import { SlSocialGithub } from "react-icons/sl";
+import { TbWorld } from "react-icons/tb";
 
 function HomePage() {
+  const Links=[
+    {
+      name:"Linked In",
+      icon:<SlSocialLinkedin />,
+      link:'http://www.linkedin.com/in/deepan-senthilkumar'
+    },
+    {
+      name:"Website",
+      icon:<TbWorld />,
+      link:''
+    },
+    {
+      name:"GitHub",
+      icon:<SlSocialGithub />,
+      link:'https://github.com/DeepanSenthilkumar005/'
+    }
+  ]
   return (
       <section className='HomePage p-4 h-screen w-screen'>
         <div className="grid stm:grid-cols-1 md:grid-cols-2">
           {/* Left Content */}
-          <div className="LeftContent">
-            <img src={ProfilePhoto} alt="" />
+          <div className="LeftContent my-auto flex justify-between">
+            <div className="links my-auto ms-12 items-center">
+              <ul className='space-y-10'>
+                {
+                  Links.map((link)=>(
+                   <li key={link}>
+                    <a href={link.link} target="_blank" rel="noopener noreferrer"><abbr title={link.name}>{link.icon}</abbr></a>
+                  </li>
+                  ))
+                }
+
+              </ul>
+            </div>
+            <img src={ProfilePhoto} alt="Profile Photo" className='h-1/2' />
           </div>
           {/* Right Content */}
-          <div className="RightContent text-white rounded-lg shadow-lg max-w-4xl mx-auto">
-            <div className="Content bg-gray-800 p-8">
+          <div className="RightContent my-auto text-white max-w-4xl mx-auto">
+            <div className="Content bg-gray-800 p-8 rounded-lg shadow-lg ">
               <b className="text-2xl font-bold">Hi there!</b>
               <p className="mt-4 text-lg">
                 I am <span className="font-semibold text-orange-500">Deepan</span>, Welcome to my creative corner! I’m a passionate designer who thrives on transforming ideas into beautiful, functional creations.
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,13 +32,22 @@ module.exports = {
       
     },
   },
+  // plugins: [
+  //   function({ addUtilities }) 
+  //   { addUtilities(
+  //     { '.no-scrollbar': { '-ms-overflow-style': 'none',/* Internet Explorer 10+ */ 
+  //      'scrollbar-width': 'none', /* Firefox */ },
+  //       '.no-scrollbar::-webkit-scrollbar': { 'display': 'none', /* Safari and Chrome */ },
+  //      });
+  //      }
+  // ],
   plugins: [
-    function({ addUtilities }) 
-    { addUtilities(
-      { '.no-scrollbar': { '-ms-overflow-style': 'none',/* Internet Explorer 10+ */ 
-       'scrollbar-width': 'none', /* Firefox */ },
-        '.no-scrollbar::-webkit-scrollbar': { 'display': 'none', /* Safari and Chrome */ },
-       });
-       }
+    function ({ addUtilities }) {
+      addUtilities({
+        '.webkit-overflow-hidden': {
+          '-webkit-overflow-scrolling': 'none',
+        },
+      });
+    },
   ],
 }
